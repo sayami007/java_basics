77edd48f10c34d97af26beae78accf7dea7e53ce
BIBESH-ADV\bbase <bmanandhar@advalent.com>
2024-07-08 21:00:55 +0545

    Dependency Injection

--- src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -1,9 +1,13 @@
 package com.in28minutes.learn_spring_framework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class GameRunner {
     private final GamingConsole game;
 
-    public GameRunner(GamingConsole game) {
+    public GameRunner(@Qualifier("SuperContraGameQualifier") GamingConsole game) {
         this.game = game;
     }
 
--- src/main/java/com/in28minutes/learn_spring_framework/game/PacmanGame.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/PacmanGame.java
@@ -1,5 +1,8 @@
 package com.in28minutes.learn_spring_framework.game;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PacmanGame implements GamingConsole {
     public void up() {
         System.out.println("Pacman Up");
