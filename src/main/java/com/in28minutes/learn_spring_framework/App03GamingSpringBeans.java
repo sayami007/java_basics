209beec392e6787bcd1b21ebb4082ab478c108d7
BIBESH-ADV\bbase <bmanandhar@advalent.com>
2024-07-08 14:09:55 +0545

    Beans Basics in Gaming Class

---
+++ src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
@@ -0,0 +1,16 @@
+package com.in28minutes.learn_spring_framework;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.GamingConsole;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class App03GamingSpringBeans {
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)) {
+            context.getBean(GamingConsole.class).up();
+            context.getBean(GameRunner.class).run();
+        }
+    }
+}
---
+++ src/main/java/com/in28minutes/learn_spring_framework/GamingConfiguration.java
@@ -0,0 +1,21 @@
+package com.in28minutes.learn_spring_framework;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.GamingConsole;
+import com.in28minutes.learn_spring_framework.game.PacmanGame;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GamingConfiguration {
+    @Bean
+    public GamingConsole game() {
+        return new PacmanGame();
+    }
+
+    @Bean
+    public GameRunner gameRunner(GamingConsole game) {
+        var gameRunner = new GameRunner(game);
+        return gameRunner;
+    }
+}
