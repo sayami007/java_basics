8f5b0098915624475584c7485951d90c3d78c25b
BIBESH-ADV\bbase <bmanandhar@advalent.com>
2024-07-08 13:41:52 +0545

    Beans

--- src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -16,7 +16,9 @@ public class App02HelloWorldSpring {
         //3: Retrieving Beans managed by Spring
         System.out.println(context.getBean("name"));
         System.out.println(context.getBean("age"));
+        System.out.println(context.getBean(Address.class));
         System.out.println(context.getBean("person"));
-        System.out.println(context.getBean("address2"));
+        System.out.println(context.getBean("person2"));
+        System.out.println(context.getBean("person3"));
     }
 }
--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,30 +3,41 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age){};
-record Address(String firstLine, String city){};
+record Person(String name, int age, Address address) {
+};
+
+record Address(String firstLine, String city) {
+};
 
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name() {
-        return "Bibesh";
+        return "Krishna";
     }
 
     @Bean
     public int age() {
-        return 20;
+        return 22;
     }
 
     @Bean
-    public Person person(){
-        return new Person("Krishna", 20);
+    public Person person() {
+        return new Person("Sudhir", 20, new Address("sdf", "sdf"));
+    }
+
+    @Bean
+    public Person person2() {
+        return new Person(name(), age(), address());
+    }
+    @Bean
+    public Person person3(String name, int age, Address address2) {
+        return new Person(name, age, address2);
     }
 
     @Bean(name = "address2")
-    public Address address(){
-        return new Address("Berkley", "MA");
+    public Address address() {
+        return new Address("Berkley", "Kathmandu");
     }
-
 }
