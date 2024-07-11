bbfe9a35484a53c875336928b1a9fd107ff8b5aa
BIBESH-ADV\bbase <bmanandhar@advalent.com>
2024-07-11 11:00:53 +0545

    Starting Spring Boot

---
+++ src/main/java/com/in28minutes/learn_spring_framework/LearnSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.in28minutes.learn_spring_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringBootApplication.class, args);
+    }
+}
